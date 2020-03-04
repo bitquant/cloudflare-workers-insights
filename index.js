3329cbaca00ed4e383d5a00a54dd338f47538884
bitquant <bitquant@bitquant.com>
2020-03-04 08:32:26 -0600

    fetch request insights and correlated logging

--- index.js
+++ index.js
@@ -8,6 +8,53 @@ async function handleRequest(params) {
     let request = event.request;
     let correlationId = getCorrelationId(request);
     let context = { correlationId: correlationId };
+    let waitUntil = (p) => event.waitUntil(p);
+
+    context.log = (data) => {
+        let logEntry = {
+            service: serviceName,
+            correlationId: correlationId,
+        };
+        Object.assign(logEntry, data);
+        return logger({ log: logEntry }, waitUntil);
+    };
+
+    context.fetch = async (request, params) => {
+
+        let fetchStart = Date.now();
+        let fetchError = null;
+        let url = (typeof request === 'string') ? request : request.url;
+        let method = (params !== undefined && params.method !== undefined) ?
+            params.method : 'GET';
+
+        let logEntry = {
+            service: serviceName,
+            correlationId: correlationId,
+            url: url,
+            method: method
+        }
+
+        try {
+            var response = await fetch(request, params);
+            logEntry.status = response.status;
+        }
+        catch(err) {
+            logEntry.status = 999;
+            logEntry.err = `${err}`
+            fetchError = err;
+        }
+
+        logEntry.duration = Date.now() - fetchStart;
+
+        logger({ fetch: logEntry }, waitUntil);
+
+        if (fetchError !== null) {
+            throw fetchError;
+        }
+
+        return response;
+    };
+
     let response = await handler(event, context);
     let endTime = Date.now();
     let duration = endTime - startTime;
@@ -23,7 +70,7 @@ async function handleRequest(params) {
         colo: request.cf.colo
     }
 
-    logger({ request: data }, (p) => event.waitUntil(p));
+    logger({ request: data }, waitUntil);
 
     return response;
 }
